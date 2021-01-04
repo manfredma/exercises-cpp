1d9a7cec963c312e647f2ba638b5efe81257e0d8
maxingfang <maxingfang@meituan.com>
2021-01-04 10:12:49 +0800

    pointer example

--- pointer_def.cpp
+++ pointer_def.cpp
@@ -12,5 +12,21 @@ int main() {
     cout << "var2 变量的地址： ";
     cout << &var2 << endl;
 
+    int  var = 20;   // 实际变量的声明
+    int  *ip;        // 指针变量的声明
+
+    ip = &var;       // 在指针变量中存储 var 的地址
+
+    cout << "Value of var variable: ";
+    cout << var << endl;
+
+    // 输出在指针变量中存储的地址
+    cout << "Address stored in ip variable: ";
+    cout << ip << endl;
+
+    // 访问指针中地址的值
+    cout << "Value of *ip variable: ";
+    cout << *ip << endl;
+
     return 0;
 }
