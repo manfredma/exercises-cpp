17a63770fd134ff3c82b5296c525e757aae24197
maxingfang <maxingfang@meituan.com>
2021-08-13 14:31:32 +0800

    signal handler

--- enum_def.cpp
+++ enum_def.cpp
@@ -5,11 +5,16 @@ using namespace std;
 int main() {
     enum color {
         red, green = 8, blue
-    } c;
+    };
 
-    c = blue;
+    color c = blue;
 
     cout << "c = " << c << endl;
 
+    enum {
+        red_2, green_2 = 10
+    };
+
+    cout << red_2 << " " << green_2 << endl;
     return 0;
 }
