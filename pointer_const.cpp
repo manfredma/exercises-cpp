cf457a19e17f130b93099ec19e3abbbf66a6147c
maxingfang <maxingfang@meituan.com>
2023-05-18 11:30:55 +0800

    test pointer

--- pointer_const.cpp
+++ pointer_const.cpp
@@ -13,12 +13,27 @@ int main() {
     int *a = (int *) malloc(i * sizeof(int));
     int *b = new int[i];
 
+    int c[2];
+    int d[2];
+
     for (int j = 0; j < i; ++j) {
         cout << a[j] << endl;
         cout << b[j] << endl;
     }
-    cout << *(a - 2)<< endl;
-    cout << *(b - 2) << endl;
+    cout << (a)<< endl;
+    cout << (b) << endl;
+    cout << "C:" << endl;
+    cout << (c) << endl;
+    cout << (c+1) << endl;
+    cout << (c+2) << endl;
+    cout << "D:" << endl;
+    cout << (d) << endl;
+    cout << (d+1) << endl;
+    cout << (d+2) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(long) << endl;
+
+
 
     return 0;
 }
